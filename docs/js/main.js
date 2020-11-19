ed48b8b55576bc65e0d44bf506f8c5b2fe0bc839
seizi.kobayasi <ir2sgepk@gmail.com>
2020-11-19 09:27:49 +0900

    設定のクッキー保存

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,9 +1,11 @@
 "use strict";
 window.onload = function () {
     calcTime();
+    loadSetting();
     window.setInterval(function () {
         calcTime();
-        calcStamina();
+        var data = getInputData();
+        calcStamina(data.maxStamina, data.parMinute);
     }, 500);
 };
 function calcTime() {
@@ -20,12 +22,45 @@ function replaceDate(date) {
         ('0' + date.getHours()).slice(-2) + ":" +
         ('0' + date.getMinutes()).slice(-2);
 }
-function calcStamina() {
+function calcStamina(maxStamina, parMinute) {
     var hour = Number(document.getElementById("hour").value);
     var minute = Number(document.getElementById("minute").value);
-    var maxStamina = Number(document.getElementById("max_stamina").value);
-    var parMinute = Number(document.getElementById("par_minute").value);
     var rest = hour * 60 + minute;
     var recovery = Math.floor(rest / parMinute);
     document.getElementById("result2").innerHTML = '' + (maxStamina - recovery);
+}
+function onTapInput() {
+    var data = getInputData();
+    saveSetting(data.maxStamina, data.parMinute);
+    calcStamina(data.maxStamina, data.parMinute);
+}
+function getInputData() {
+    var maxStamina = Number(document.getElementById("max_stamina").value);
+    var parMinute = Number(document.getElementById("par_minute").value);
+    return {
+        maxStamina: maxStamina,
+        parMinute: parMinute
+    };
+}
+var COOKIE_TAG = {
+    MAX_STAMINA: "max_stamina",
+    PAR_MINUTE: "par_minute"
+};
+function saveSetting(maxStamina, parMinute) {
+    document.cookie = COOKIE_TAG.MAX_STAMINA + "=" + maxStamina;
+    document.cookie = COOKIE_TAG.PAR_MINUTE + "=" + parMinute;
+}
+function loadSetting() {
+    var cookieList = document.cookie.split(";");
+    var map = {};
+    cookieList.forEach(function (cookie) {
+        var key_vlue = cookie.split("=");
+        map[key_vlue[0].trim()] = key_vlue[1];
+    });
+    if (map[COOKIE_TAG.MAX_STAMINA]) {
+        document.getElementById("max_stamina").value = map[COOKIE_TAG.MAX_STAMINA];
+    }
+    if (map[COOKIE_TAG.PAR_MINUTE]) {
+        document.getElementById("par_minute").value = map[COOKIE_TAG.PAR_MINUTE];
+    }
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,8 +1,11 @@
 window.onload = () => {
 	calcTime();
+	loadSetting();
+
 	window.setInterval(() => {
 		calcTime();
-		calcStamina();
+		let data = getInputData();
+		calcStamina(data.maxStamina, data.parMinute);
 	}, 500);
 }
 
@@ -25,11 +28,9 @@ function replaceDate(date : Date) : string {
 		('0' + date.getMinutes()).slice(-2);
 }
 
-function calcStamina() : void {
+function calcStamina(maxStamina : number, parMinute : number) : void {
 	let hour = Number((<HTMLInputElement>document.getElementById("hour")).value);
 	let minute = Number((<HTMLInputElement>document.getElementById("minute")).value);
-	let maxStamina = Number((<HTMLInputElement>document.getElementById("max_stamina")).value);
-	let parMinute = Number((<HTMLInputElement>document.getElementById("par_minute")).value);
 
 	// 残り時間(分)
 	let rest = hour * 60 + minute;
@@ -38,4 +39,50 @@ function calcStamina() : void {
 	let recovery = Math.floor(rest / parMinute);
 	
 	(<HTMLElement>document.getElementById("result2")).innerHTML = '' + (maxStamina - recovery);
+}
+
+
+function onTapInput() : void {
+	let data = getInputData();
+
+	// 設定保存
+	saveSetting(data.maxStamina, data.parMinute);
+
+	calcStamina(data.maxStamina, data.parMinute);
+}
+
+function getInputData() {
+	let maxStamina = Number((<HTMLInputElement>document.getElementById("max_stamina")).value);
+	let parMinute = Number((<HTMLInputElement>document.getElementById("par_minute")).value);
+
+	return {
+		maxStamina: maxStamina,
+		parMinute: parMinute
+	};
+}
+
+const COOKIE_TAG = {
+	MAX_STAMINA: "max_stamina",
+	PAR_MINUTE: "par_minute"
+};
+
+function saveSetting(maxStamina: number, parMinute: number) : void{
+	document.cookie = COOKIE_TAG.MAX_STAMINA + "=" + maxStamina;
+	document.cookie = COOKIE_TAG.PAR_MINUTE + "=" + parMinute;
+}
+
+function loadSetting() : void {
+	let cookieList = document.cookie.split(";");
+	let map : {[index: string]: string} = {};
+	cookieList.forEach((cookie) => {
+		let key_vlue = cookie.split("=");
+		map[key_vlue[0].trim()] = key_vlue[1];
+	});
+
+	if(map[COOKIE_TAG.MAX_STAMINA]) {
+		(<HTMLInputElement>document.getElementById("max_stamina")).value = map[COOKIE_TAG.MAX_STAMINA];
+	}
+	if(map[COOKIE_TAG.PAR_MINUTE]) {
+		(<HTMLInputElement>document.getElementById("par_minute")).value = map[COOKIE_TAG.PAR_MINUTE];
+	}
 }
